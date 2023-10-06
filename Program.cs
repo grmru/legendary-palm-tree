fdb63de6467ddb295c6754a90f23f7e15104ef8c
Георгий Цырков <g@tias.pro>
2023-10-06 16:42:27 +0300

    rewrite first class with usings

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace PalmTree;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hi");
+    }
+}

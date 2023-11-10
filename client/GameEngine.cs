89cd1eb1c41c7d3261fa5c78d76d10d4a95b1003
Георгий Цырков <g@tias.pro>
2023-11-10 16:51:17 +0300

    client folder

--- client/GameEngine.cs
+++ client/GameEngine.cs
@@ -11,6 +11,11 @@ using Microsoft.VisualBasic;
 using System.Net.NetworkInformation;
 using System.Security.Cryptography.X509Certificates;
 using System.Collections.Specialized;
+using System.Net.Sockets;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
+using System.IO;
 
 namespace PalmTree.Engine;
 
@@ -137,9 +142,45 @@ public class GameEngine
     {
         while(isRunning)
         {
-            //Get Players
-            // _p_data = new WebClient().DownloadString(_mainUrl + "mmo/getPlayers/" + NickName + "-" + _player.X + "-" + _player.Y);
-            _p_data = new WebClient().DownloadString(_mainUrl + "mmo/getPlayers/");
+
+            string currentPlayerData = NickName + "-" + _player.X + "-" + _player.Y;
+            Byte[] data = System.Text.Encoding.UTF8.GetBytes(currentPlayerData);
+
+            Int32 port = 1234;
+            TcpClient client = new TcpClient("127.0.0.1", port);
+
+            NetworkStream stream = client.GetStream();
+
+            // Send the message to the connected TcpServer.
+            stream.Write(data, 0, data.Length);
+
+
+            List<byte> bytes = new List<byte>();
+            if (stream.CanRead)
+            {
+                byte[] readBuffer = new byte[1024];
+                do
+                {
+                    try
+                    {
+                        int count = stream.Read(readBuffer, 0, readBuffer.Length);
+                        byte[] rData = new byte[count];
+                        for (int i = 0; i < count; i++)
+                        {
+                            rData[i] = readBuffer[i];
+                        }
+                        bytes.AddRange(rData);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Write("[ERROR]: " + ex.ToString());
+                    }
+                    System.Threading.Thread.Sleep(1);
+                }
+                while (stream.DataAvailable);
+            }
+
+            _p_data = System.Text.Encoding.UTF8.GetString(bytes.ToArray());
 
             //Split Players
             string[] temp_p_data = _p_data.Split('_');
@@ -162,7 +203,7 @@ public class GameEngine
                     other_players.Add(new Item(temp_p_var[0], '@', int.Parse(temp_p_var[1]), int.Parse(temp_p_var[2])));
             }
 
-            System.Threading.Thread.Sleep(50);
+            System.Threading.Thread.Sleep(30);
         }
     }
 
@@ -288,18 +329,19 @@ public class GameEngine
                 break;
         }
 
+        /*
         if(onlineMode){
-
             System.Threading.Thread th = new Thread(SendPlayerData);
             th.Start();
         }
+        */
     }
 
-    public void SendPlayerData()
-    {
-        //Send local pos to server
-        new WebClient().DownloadString(_mainUrl + "mmo/move/" + NickName + "-" + _player.X + "-" + _player.Y);
-    }
+    //public void SendPlayerData()
+    //{
+    //    //Send local pos to server
+    //    new WebClient().DownloadString(_mainUrl + "mmo/move/" + NickName + "-" + _player.X + "-" + _player.Y);
+    //}
 
     
 
@@ -318,7 +360,7 @@ public class GameEngine
         File.WriteAllText("log.txt", r);
 
         //Delete from server
-        if(onlineMode) new WebClient().DownloadString(_mainUrl + "mmo/kill/" + NickName);
+        //if(onlineMode) new WebClient().DownloadString(_mainUrl + "mmo/kill/" + NickName);
 
         Console.WriteLine("Exiting...");
         cts.Cancel();
